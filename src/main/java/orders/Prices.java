e0ac7bba5651ea6c0fe9c3dcbe4040cd77c9498f
Uladzislau_Malchanau <uladzislau_malchanau@epam.com>
2018-04-26 11:52:46 +0300

    fix prices

--- src/main/java/orders/Prices.java
+++ src/main/java/orders/Prices.java
@@ -1,30 +1,31 @@
 package orders;
 
 public class Prices {
-    private final int[] prices;
+    private int[] prices;
+    private int   range;
 
-    public Prices(final int size) {
-        prices = new int[size];
+    public Prices(final int range) {
+        this.range = range;
     }
 
     public void increase(int price, int count) {
-//        System.out.println("increase: price = " + price + ", count = " + count);
         put(price, count);
     }
 
     public void decrease(int price, int count) {
-//        System.out.println("decrease: price = " + price + ", count = " + count);
         put(price, -count);
     }
 
     public int getPrice(int price) {
-        return prices[idx(price)];
+        return prices == null ? 0 : prices[idx(price)];
     }
 
     private void put(final int price, final int count) {
-//        if (price == 649) {
-//            System.out.println();
-//        }
+        if (prices == null) {
+            int maxArraySize = (int) Long.min(Integer.MAX_VALUE, (long) price + range);
+            int minArraySize = Integer.max(0, price - range);
+            prices = new int[maxArraySize - minArraySize + 1];
+        }
         prices[idx(price)] += count;
     }
 
